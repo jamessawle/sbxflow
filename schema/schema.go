39b30d80d09c5304b492f86d3c77f862cbf1a4ca
James Sawle <jamessawle@hotmail.com>
2026-08-16 11:57:13 +0100

    docs(schema): explain embedded schema import

--- schema/schema.go
+++ schema/schema.go
@@ -1,6 +1,8 @@
 // Package schema exposes the published sbxflow configuration schema.
 package schema
 
+// Import embed for the //go:embed directive below without adding its API to
+// this package's namespace.
 import _ "embed"
 
 // Configuration contains the Draft 2020-12 schema for sbxflow.yaml.
